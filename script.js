a68c7569c2d4791a2f03c0fd1404d740c11ed799
AntoninaVi <tonatonotonya@gmail.com>
2022-02-18 16:24:28 +0300

    mobile version start

---
+++ script.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+    $('.slider').slick({
+        boolean: true,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            
+        ]
+    });
+});
