24e118c26d0bac8893b2859af5772ca3b9444b23
AntoninaVi <tonatonotonya@gmail.com>
2022-02-20 22:04:23 +0300

    slider, burger-menu

--- script.js
+++ script.js
@@ -8,7 +8,18 @@ $(document).ready(function () {
         slidesToShow: 1,
         slidesToScroll: 1,
         responsive: [
-            
+
         ]
+
+    })
+});
+
+$(function () {
+    $(".burger__btn").on("click", function () {
+        $(".header__nav").slideToggle(200, function () {
+            if ($(this).css("display") === "none") {
+                $(this).removeAttr("style");
+            }
+        });
     });
 });
